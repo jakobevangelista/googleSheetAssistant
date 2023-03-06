703d8fa2e0b7f4bd42a7cf61f6fb2a1018bed62f
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-03-06 02:01:13 -0600

    added basic sign in with google

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider
+      // Provider options are not required but can be useful in situations where
+      // you have a short session maxAge time. Shown here with default values.
+      session={pageProps.session}
+    >
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,42 @@
+import PocketBase from "pocketbase";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        <div className="flex">
+          <div className="flex flex-col w-1/6 bg-[#343541] h-screen">
+            <div className="text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer">
+              bots.ai
+            </div>
+            <div className="text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer">
+              Signed in as {session.user.email}
+            </div>
+            <div className="text-center py-3 px-3 rounded-md mt-64 text-white hover:bg-grey-500  cursor-pointer">
+              Settings
+            </div>
+            <button onClick={() => signOut()}>Sign out</button>
+            <div className=""></div>
+          </div>
+          <div className="flex flex-col w-5/6 bg-[#202123] h-screen"></div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      <div className='flex'>
-        <div className='flex flex-col w-1/6 bg-[#343541] h-screen'>
-          <div className='text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer'>
+      <div className="flex">
+        <div className="flex flex-col w-screen bg-[#202123] h-screen">
+          <div className="text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer">
             bots.ai
           </div>
-          <div className='text-center py-3 px-3 rounded-md mt-64 text-white hover:bg-grey-500  cursor-pointer'>
-            Settings
+          <div className="text-center py-3 px-3 rounded-md mt-64 text-white hover:bg-grey-500  cursor-pointer">
+            <button onClick={() => signIn()}>Sign in</button>
           </div>
-          <div className=''></div>
         </div>
-        <div className='flex flex-col w-5/6 bg-[#202123] h-screen'></div>
       </div>
     </>
   );
