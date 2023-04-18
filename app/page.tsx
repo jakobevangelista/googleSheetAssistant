7bb52cd158ea59de9af0b2e91208b4aae0c91e27
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-04-18 13:14:25 -0500

    cleaned up frontend, added custom font size, added custom sheet link

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useState } from "react";
 import ChatMessage from "../components/Chatmessage";
+import Link from "next/link";
 
 // import { useSession, signIn, signOut } from "next-auth/react";
 
@@ -11,21 +12,31 @@ const pb = new PocketBase("http://45.33.6.9:80");
 
 export default function Home() {
   const router = useRouter();
-  const [username, setUsername] = useState("");
+  // const [username, setUsername] = useState("");
   const [userInput, setUserInput] = useState("");
+  const [sheetLink, setSheetLink] = useState("");
+  const [fontSize, setFontSize] = useState("flex");
+  const [realLink, setRealLink] = useState(
+    "https://docs.google.com/spreadsheets/d/11oC81VbhDhRqE8NY2ZNrlEXIrdsAummmLxihhPqctmw/edit#gid=0"
+  );
   const [chatLog, setChatLog] = useState([
     {
       role: "system",
       content: `You are a bot that is an expert at google sheets and helps users out with all things related to google sheets.`,
     },
   ]);
+  const [darkMode, setDarkMode] = useState(true);
 
-  useEffect(() => {
-    if (!pb.authStore.isValid) {
-      router.push("/login");
-    }
-    setUsername(pb.authStore.model.username);
-  }, [router]);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  // useEffect(() => {
+  //   if (!pb.authStore.model) {
+  //     router.push("/login");
+  //   }
+  //   setUsername(pb.authStore.model.username);
+  // }, [router]);
 
   function logout() {
     pb.authStore.clear();
@@ -36,13 +47,17 @@ export default function Home() {
     event.preventDefault();
     let chatLogNew = [...chatLog, { role: "user", content: `${userInput}` }];
 
-    const response = await fetch("http://127.0.0.1:5000/fix", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message: userInput }), // takes only the user input for now, use more advanced prompt engineering to mimic chatgpt
-    });
+    const response = await fetch(
+      // "https://botaiwebsitebackend.herokuapp.com/fix",
+      "http://127.0.0.1:5000/fix",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: userInput }), // takes only the user input for now, use more advanced prompt engineering to mimic chatgpt
+      }
+    );
     const data = await response.json();
     console.log(data);
     setUserInput("");
@@ -50,6 +65,11 @@ export default function Home() {
       ...chatLogNew,
       { role: "assistant", content: `${data.content}` },
     ]);
+  }
+
+  async function handleSheetLink(event) {
+    event.preventDefault();
+    setRealLink(sheetLink);
   }
 
   function clearChat() {
@@ -60,30 +80,58 @@ export default function Home() {
       },
     ]);
   }
+  function increaseTextSize() {
+    setFontSize("flex text-xl");
+  }
+  function decreaseTextSize() {
+    setFontSize("flex text-sm");
+  }
 
   return (
     <>
-      <div className="flex">
+      {/* <div className={`${darkMode ? "dark" : ""}`}> */}
+      <div className={`${darkMode ? "dark" : ""} ${fontSize}`}>
         <div className="flex flex-col w-1/6 bg-[#343541] h-screen">
           <div className="text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer">
             bots.ai
           </div>
-          <div className="text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer">
-            Signed in as {username}
-          </div>
           <a
-            className="text-center py-3 px-3 rounded-md hover:bg-gray-500 border border-white cursor-pointer"
+            className="text-center py-3 px-3 rounded-md hover:bg-gray-500 border border-white text-white cursor-pointer"
             onClick={clearChat}
           >
             New Chat
           </a>
-          <div className="text-center py-3 px-3 rounded-md mt-64 text-white hover:bg-grey-500  cursor-pointer">
+          <div className="text-center py-3 px-3 rounded-md mt-64 text-white">
             Settings
           </div>
-          <button onClick={logout}>Sign out</button>
-          <div className=""></div>
+          <form onSubmit={handleSheetLink} className="p-3">
+            <input
+              className="w-full bg-[#40414f] rounded-md text-white outline-none p-3"
+              value={sheetLink}
+              onChange={(e) => setSheetLink(e.target.value)}
+              placeholder="Input sheet link here"
+            ></input>
+          </form>
+          <a
+            className="text-center py-3 px-3 rounded-md hover:bg-gray-500 border border-white text-white cursor-pointer"
+            onClick={increaseTextSize}
+          >
+            Increase Text Size
+          </a>
+          <a
+            className="text-center py-3 px-3 rounded-md hover:bg-gray-500 border border-white text-white cursor-pointer"
+            onClick={decreaseTextSize}
+          >
+            Decrease Text Size
+          </a>
+          <button
+            className="text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-white mx-2"
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? "Light Mode" : "Dark Mode"}
+          </button>
         </div>
-        <div className="flex flex-row w-5/6 bg-[#202123] h-screen">
+        <div className="flex flex-row w-5/6 bg-white dark:bg-[#202123] h-screen">
           <div className="flex-col overflow-y-auto w-1/2 h-screen">
             <div className="flex overflow-y-auto relative flex-col pb-14">
               {chatLog.map((content, index) => (
@@ -93,7 +141,7 @@ export default function Home() {
             <div className="p-3 bottom-0">
               <form onSubmit={handleSubmit}>
                 <input
-                  className="w-full bg-[#40414f] rounded-md text-white outline-none p-3 text-sm"
+                  className="w-full bg-[#40414f] rounded-md text-white outline-none p-3"
                   value={userInput}
                   onChange={(e) => setUserInput(e.target.value)}
                   placeholder="Ask me anything"
@@ -102,11 +150,12 @@ export default function Home() {
             </div>
           </div>
           <iframe
-            className="px-16 py-64 bg-[#202123] w-1/2 h-screen"
-            src="https://docs.google.com/spreadsheets/d/11oC81VbhDhRqE8NY2ZNrlEXIrdsAummmLxihhPqctmw/edit#gid=0"
+            className="px-8 py-16 bg-[#202123] w-1/2 h-screen"
+            src={realLink}
           ></iframe>
         </div>
       </div>
+      {/* </div> */}
     </>
   );
 }
---
+++ app/settings/page.jsx
@@ -0,0 +1,14 @@
+export default async function SettingsPage() {
+  return (
+    <>
+      <div className="flex">
+        <div className="flex flex-col w-1/6 bg-[#343541] h-screen">
+          <div className="text-center py-3 px-3 rounded-md hover:underline text-white cursor-pointer">
+            bots.ai setting page
+          </div>
+        </div>
+        <div className="flex flex-row w-5/6 bg-[#202123] h-screen"></div>
+      </div>
+    </>
+  );
+}
